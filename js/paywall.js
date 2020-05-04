45e1bfdf3284ac33cd8bd3bd7f572f15bd69973c
Stef Naskova <snaskova094@gmail.com>
2020-05-04 13:14:58 +0200

    Revert "Added the paywall init here (before all scripts) for the static asset id (97670) on this page"
    This reverts commit dad938df737fa9926dd6846a3b1327a3dcb208e5.

--- js/paywall.js
+++ js/paywall.js
@@ -1,26 +1,17 @@
-function getParameterByName(name, url) {
-	if (!url) url = window.location.href;
-	name = name.replace(/[\[\]]/g, "\\$&");
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return "";
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-var paywall = new InplayerPaywall("3da670ca-d4c8-4c57-8511-c5c90562fc27", [
-	{
-		id: getParameterByName('id')
-
-	},
-]);
-
 var config_ip = {
 	packages: [97576, 97577, 97578, 97579],
 	service_url: "https://services.inplayer.com",
 };
 
-
+var paywall = new InplayerPaywall("3da670ca-d4c8-4c57-8511-c5c90562fc27", [
+	{
+		id: 97670,
+		options: {
+			noPreview: true,
+			noInject: true,
+		},
+	},
+]);
 // {
 //   oauthAppKey: '17c128af636af78c894d217a783c07dc',
 //   brandingId: 823
@@ -51,14 +42,22 @@ function createItemElement(assetId, assetPhoto, assetTitle) {
 	return output;
 }
 
+function getParameterByName(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return "";
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
 $(function () {
-
-	// console.log(getParameterByName('id'))
-
-	$('#preview-item').html(`<div id="inplayer-${getParameterByName('id')}" class="inplayer-paywall"></div>`);
-
-
+	$("#preview-item").html(
+		'<div id="inplayer-' +
+			getParameterByName("id") +
+			'" class="inplayer-paywall"></div>'
+	);
 
 	$(".inplayer-paywall-logout").parent().hide();
 
@@ -79,9 +78,9 @@ $(function () {
 
 			$.get(
 				config_ip.service_url +
-				"/items/packages/" +
-				package +
-				"/items?limit=500",
+					"/items/packages/" +
+					package +
+					"/items?limit=500",
 				(response) => {
 					var output = "";
 					packageNumber++;
